62435cea540d03d50b3624386f223367c3211204
Daniel-Villar <a.22308051281330@cbtis128.edu.mx>
2024-09-20 09:38:08 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,49 @@
+print("Activdad 9 Clases V3 ")
+print("Daniel Villar 22308051281330")
+# zona de clase 
+class operadores1330:
+    def suma1330(self,D,V):
+        s1330= D+V
+        print(f"La suma de {D} + {V} = {s1330}")
+    #resta
+    def resta1330(self,A,F):
+        d1330= A-F
+        print(f"La resta de {A}-{F} = {d1330}")
+    #multiplicaciion
+    def multi1330(self,B,C):
+        A1330= B*C
+        print(f"La multiplicacion de {B}*{C} = {A1330}")
+    #division
+    def divi1330(self,Q,W):
+        G1330= Q/W
+        print(f"La division de {Q}/{W} = {G1330}")
+    #modulo
+    def modulo1330(self,o,p):
+        f1330= o%p
+        print(f"El modulo de {o}%{p} = {f1330}")
+    #exponente
+    def expo1330(self,z,x):
+        h1330= z**x
+        print(f"El exponente de {z}**{x} = {h1330}")
+    #cociente
+    def cociente1330(self,n,m):
+        k1330= n//m
+        print(f"El cociente de {n}//{m} = {k1330}")
+    #zona de creacion de objeto
+OperaAlexis=operadores1330()    
+
+#zona de uso de objetos 
+print("funcion suma")
+OperaAlexis.suma1330(5,4)
+print("Funcion resta")
+OperaAlexis.resta1330(5,4)
+print("Funcion Multiplicacion")
+OperaAlexis.multi1330(5,4)
+print("Funcion Division")
+OperaAlexis.divi1330(5,4)
+print("Funcion Modulo")
+OperaAlexis.modulo1330(5,4)
+print("Funcion exponiente")
+OperaAlexis.expo1330(5,4)
+print("Funcion cociente")
+OperaAlexis.cociente1330(5,4)
